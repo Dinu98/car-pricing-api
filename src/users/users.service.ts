50584fcba3df9c4a973d0d215d811d14389be6bd
Dinu98 <alexdinescu888@gmail.com>
2025-02-18 15:25:48 +0200

    Using usersRepository to create and save users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.usersRepository.create({ email, password });
+
+    return this.usersRepository.save(user);
+  }
+}
