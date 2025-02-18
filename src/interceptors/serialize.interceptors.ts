88e96f937efbda9c9a6e2a1516765efb8498e203
Dinu98 <alexdinescu888@gmail.com>
2025-02-18 18:43:47 +0200

    Modifying interceptor to take any DTO we pass to it

--- src/interceptors/serialize.interceptors.ts
+++ src/interceptors/serialize.interceptors.ts
@@ -1,10 +1,11 @@
-import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
-import { ReturnUserDto } from 'src/users/dtos/return-user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
     //Run before request is handled by route handler
     //console.log('Before request handling: ', context);
@@ -13,7 +14,7 @@ export class SerializeInterceptor implements NestInterceptor {
       map((data: any) => {
         //Run before response is sent
         //console.log('Before response is sent: ', data);
-        return plainToClass(ReturnUserDto, data, {
+        return plainToClass(this.dto, data, {
           //This setting is making sure that only properties
           //Marked with @Expose() are gonna be kept
           excludeExtraneousValues: true
